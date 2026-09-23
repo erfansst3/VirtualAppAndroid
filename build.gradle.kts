a95315b1b549a8f1c962504d4ce8b6fc9ca366e1
mostwanted <ash.luna.shei@gmail.com>
2026-09-23 22:57:21 +0330

    Simplify root Gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,18 +3,3 @@ id("com.android.application") version "8.13.0" apply false
 id("com.android.library") version "8.13.0" apply false
 id("org.jetbrains.kotlin.android") version "2.2.21" apply false
 }
-subprojects{
-plugins.withId("com.android.application"){
-extensions.configure<com.android.build.gradle.AppExtension>{
-compileOptions{sourceCompatibility=JavaVersion.VERSION_17;targetCompatibility=JavaVersion.VERSION_17}
-}
-}
-plugins.withId("com.android.library"){
-extensions.configure<com.android.build.gradle.LibraryExtension>{
-compileOptions{sourceCompatibility=JavaVersion.VERSION_17;targetCompatibility=JavaVersion.VERSION_17}
-}
-}
-plugins.withId("org.jetbrains.kotlin.android"){
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach{compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)}
-}
-}
