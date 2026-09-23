dbd5029816e04f629bd4c842bf250983ee8bdfc9
mostwanted <ash.luna.shei@gmail.com>
2026-09-23 23:11:21 +0330

    Load base and split APK dex and native libraries

--- virtual-core/src/main/java/com/erfansst/virtual/core/VirtualClassLoader.kt
+++ virtual-core/src/main/java/com/erfansst/virtual/core/VirtualClassLoader.kt
@@ -5,7 +5,8 @@ import java.io.File
 class VirtualClassLoader(private val context:Context){
 fun load(clone:CloneInfo):DexClassLoader{
 val opt=File(clone.dataDir,"dex");opt.mkdirs()
-val lib=File(clone.dataDir,"lib");lib.mkdirs()
-return DexClassLoader(clone.apk.path,opt.path,lib.path,context.classLoader)
+val lib=VirtualNativeLibs.extract(clone.apk,File(clone.dataDir,"lib"))
+val paths=(listOf(clone.apk.path)+clone.splits.map{it.path}).joinToString(File.pathSeparator)
+return DexClassLoader(paths,opt.path,lib,context.classLoader)
 }
 }
