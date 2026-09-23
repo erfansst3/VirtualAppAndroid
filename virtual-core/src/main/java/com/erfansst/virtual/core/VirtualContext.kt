8d3e5719684562476e8af7a6159b45f4e2c29eec
mostwanted <ash.luna.shei@gmail.com>
2026-09-23 23:12:38 +0330

    Expand isolated virtual filesystem context

--- virtual-core/src/main/java/com/erfansst/virtual/core/VirtualContext.kt
+++ virtual-core/src/main/java/com/erfansst/virtual/core/VirtualContext.kt
@@ -11,11 +11,16 @@ override fun getPackageName()=appInfo.packageName
 override fun getClassLoader()=loader
 override fun getResources()=vr
 override fun getApplicationInfo()=appInfo
+override fun getApplicationContext():Context=this
+override fun getPackageCodePath()=clone.apk.path
+override fun getPackageResourcePath()=clone.apk.path
 override fun getDataDir()=root
 override fun getFilesDir()=File(root,"files").also{it.mkdirs()}
 override fun getCacheDir()=File(root,"cache").also{it.mkdirs()}
 override fun getCodeCacheDir()=File(root,"code_cache").also{it.mkdirs()}
 override fun getNoBackupFilesDir()=File(root,"no_backup").also{it.mkdirs()}
+override fun getExternalFilesDir(type:String?)=File(root,"external_files"+(if(type==null)"-root" else "/$type")).also{it.mkdirs()}
+override fun getExternalCacheDir()=File(root,"external_cache").also{it.mkdirs()}
 override fun getDatabasePath(name:String)=File(root,"databases/$name").also{it.parentFile?.mkdirs()}
 override fun getDir(name:String,mode:Int)=File(root,"app_$name").also{it.mkdirs()}
 }
