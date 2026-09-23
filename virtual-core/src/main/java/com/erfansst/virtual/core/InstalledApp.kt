784612b7d4eae6d8bd82b1eb6d0b4970ba85f411
mostwanted <ash.luna.shei@gmail.com>
2026-09-23 23:05:41 +0330

    Add installed app and clone runtime foundation

---
+++ virtual-core/src/main/java/com/erfansst/virtual/core/InstalledApp.kt
@@ -0,0 +1,3 @@
+package com.erfansst.virtual.core
+import android.content.pm.ApplicationInfo
+data class InstalledApp(val packageName:String,val label:String,val versionName:String,val appInfo:ApplicationInfo)
