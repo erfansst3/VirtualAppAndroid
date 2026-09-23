bdeabd643495aefede3ee37acce7e47feb18aa37
mostwanted <ash.luna.shei@gmail.com>
2026-09-23 23:11:18 +0330

    Resolve installed launcher activities

--- virtual-core/src/main/java/com/erfansst/virtual/core/VirtualPackageManager.kt
+++ virtual-core/src/main/java/com/erfansst/virtual/core/VirtualPackageManager.kt
@@ -7,7 +7,7 @@ val pm=context.packageManager
 val i=Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_LAUNCHER)
 return pm.queryIntentActivities(i,0).map{r->
 val a=r.activityInfo.applicationInfo
-InstalledApp(a.packageName,pm.getApplicationLabel(a).toString(),pm.getPackageInfo(a.packageName,0).versionName.orEmpty(),a)
+InstalledApp(a.packageName,pm.getApplicationLabel(a).toString(),pm.getPackageInfo(a.packageName,0).versionName.orEmpty(),a,r.activityInfo.name)
 }.filter{it.packageName!=context.packageName}.distinctBy{it.packageName}.sortedBy{it.label.lowercase()}
 }
 }
