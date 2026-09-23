6b20ac3d5a488e045f3c9f9a78b8e9742b5e3960
mostwanted <ash.luna.shei@gmail.com>
2026-09-23 23:14:06 +0330

    Load resources from base and split APKs

--- virtual-core/src/main/java/com/erfansst/virtual/core/VirtualResources.kt
+++ virtual-core/src/main/java/com/erfansst/virtual/core/VirtualResources.kt
@@ -2,9 +2,9 @@ package com.erfansst.virtual.core
 import android.content.res.AssetManager
 import android.content.res.Resources
 class VirtualResources{
-fun open(base:Resources,apk:String):Resources{
+fun open(base:Resources,paths:List<String>):Resources{
 val a=AssetManager()
-a.addAssetPath(apk)
+paths.distinct().forEach{a.addAssetPath(it)}
 return Resources(a,base.displayMetrics,base.configuration)
 }
 }
