0f6b9e0984e050ebebd83382a34b800e2287f8b4
mostwanted <ash.luna.shei@gmail.com>
2026-09-23 23:21:14 +0330

    Fix virtual runtime build errors

--- virtual-core/src/main/java/com/erfansst/virtual/core/VirtualResources.kt
+++ virtual-core/src/main/java/com/erfansst/virtual/core/VirtualResources.kt
@@ -3,8 +3,12 @@ import android.content.res.AssetManager
 import android.content.res.Resources
 class VirtualResources{
 fun open(base:Resources,paths:List<String>):Resources{
-val a=AssetManager()
-paths.distinct().forEach{a.addAssetPath(it)}
-return Resources(a,base.displayMetrics,base.configuration)
+val a=AssetManager::class.java.getDeclaredConstructor()
+a.isAccessible=true
+val am=a.newInstance()
+val add=AssetManager::class.java.getDeclaredMethod("addAssetPath",String::class.java)
+add.isAccessible=true
+paths.distinct().forEach{add.invoke(am,it)}
+return Resources(am,base.displayMetrics,base.configuration)
 }
 }
