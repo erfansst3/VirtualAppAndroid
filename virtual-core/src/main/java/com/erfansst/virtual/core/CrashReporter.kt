42410dbbcc14c03428c1f73d39394b363c4601c2
mostwanted <ash.luna.shei@gmail.com>
2026-09-23 23:26:02 +0330

    Record clone identity in crash diagnostics

--- virtual-core/src/main/java/com/erfansst/virtual/core/CrashReporter.kt
+++ virtual-core/src/main/java/com/erfansst/virtual/core/CrashReporter.kt
@@ -12,6 +12,9 @@ val old=Thread.getDefaultUncaughtExceptionHandler()
 Thread.setDefaultUncaughtExceptionHandler{t,e->
 f.parentFile?.mkdirs()
 val d=SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS",Locale.US).format(Date())
-f.appendText(d+" "+Build.MODEL+" "+Build.VERSION.SDK_INT+" "+t+"\n"+e.stackTraceToString()+"\n")
+val s=VirtualSessionManager.current()
+val line=d+" "+Build.MODEL+" "+Build.VERSION.SDK_INT+" "+t+" clone="+(s?.clone?.packageName?:"host")+"#"+(s?.clone?.cloneId?:0)+"\n"+e.stackTraceToString()+"\n"
+f.appendText(line)
+s?.let{VirtualLogStore.write(context,it.clone,"CRASH "+e.stackTraceToString())}
 old?.uncaughtException(t,e)
 }}}
