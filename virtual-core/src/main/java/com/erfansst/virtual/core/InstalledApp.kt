5d8bc09fb85cfdc0e9956ec12445e29f47ea5ad4
mostwanted <ash.luna.shei@gmail.com>
2026-09-23 23:11:16 +0330

    Track launcher component for cloned apps

--- virtual-core/src/main/java/com/erfansst/virtual/core/InstalledApp.kt
+++ virtual-core/src/main/java/com/erfansst/virtual/core/InstalledApp.kt
@@ -1,3 +1,3 @@
 package com.erfansst.virtual.core
 import android.content.pm.ApplicationInfo
-data class InstalledApp(val packageName:String,val label:String,val versionName:String,val appInfo:ApplicationInfo)
+data class InstalledApp(val packageName:String,val label:String,val versionName:String,val appInfo:ApplicationInfo,val launcherActivity:String)
